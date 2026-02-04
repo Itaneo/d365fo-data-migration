b00632617050affa71e9a6a61c00d32c7cd0f106
Jerome Piquot <jpiquot@itaneo.com>
2026-02-04 07:51:00 +0000

    Update logging level to Debug in appSettings.json and refactor Dynamics365FnoClient methods for improved readability

--- Dynamics365ImportData/Dynamics365ImportData/Erp/DataManagementDefinitionGroups/Dynamics365FinanceDataManagementGroups.cs
+++ Dynamics365ImportData/Dynamics365ImportData/Erp/DataManagementDefinitionGroups/Dynamics365FinanceDataManagementGroups.cs
@@ -11,34 +11,31 @@ using System.Threading.Tasks;
 
 public class Dynamics365FinanceDataManagementGroups : Dynamics365FnoClient, IDynamics365FinanceDataManagementGroups
 {
-    public Dynamics365FinanceDataManagementGroups(IOptions<Dynamics365Settings> settings, HttpClient client, ILogger<Dynamics365FinanceDataManagementGroups> logger)
-        : base(nameof(DataManagementDefinitionGroups), settings, client, logger)
+	public Dynamics365FinanceDataManagementGroups(IOptions<Dynamics365Settings> settings, HttpClient client, ILogger<Dynamics365FinanceDataManagementGroups> logger)
+		: base(nameof(DataManagementDefinitionGroups), settings, client, logger)
 
-    {
-    }
+	{
+	}
 
-    public async Task<BlobDefinition> GetAzureWriteUrl(string uniqueFileName, CancellationToken cancellationToken = default)
-    {
-        return await PostRequestAsync<UniqueFileNameRequest, BlobDefinition>(nameof(GetAzureWriteUrl),
-                                                                                new UniqueFileNameRequest(uniqueFileName),
-                                                                                cancellationToken);
-    }
+	public async Task<BlobDefinition> GetAzureWriteUrl(string uniqueFileName, CancellationToken cancellationToken = default) => await PostRequestAsync<UniqueFileNameRequest, BlobDefinition>(nameof(GetAzureWriteUrl),
+																				new UniqueFileNameRequest(uniqueFileName),
+																				cancellationToken);
 
-    public async Task<ExecutionStatus> GetExecutionSummaryStatus(string executionId, CancellationToken cancellationToken = default)
-    {
-        string status = await PostRequestAsync<ExecutionIdRequest, string>(nameof(GetExecutionSummaryStatus),
-                                                                                new ExecutionIdRequest(executionId),
-                                                                                cancellationToken);
-        return Enum.Parse<ExecutionStatus>(status);
-    }
+	public async Task<ExecutionStatus> GetExecutionSummaryStatus(string executionId, CancellationToken cancellationToken = default)
+	{
+		string status = await PostRequestAsync<ExecutionIdRequest, string>(nameof(GetExecutionSummaryStatus),
+																				new ExecutionIdRequest(executionId),
+																				cancellationToken);
+		return Enum.Parse<ExecutionStatus>(status);
+	}
 
-    public async Task<string> ImportFromPackage(ImportFromPackageRequest parameters, CancellationToken cancellationToken = default)
-    {
-        string executionId = await PostRequestAsync<ImportFromPackageRequest, string>(nameof(ImportFromPackage),
-                                                                                parameters,
-                                                                                cancellationToken);
-        return executionId != parameters.ExecutionId
-            ? throw new Exception($"The returned execution id ({executionId}), is not the same as the one defined in the parameters : {parameters.ExecutionId}.\nParameters : {JsonSerializer.Serialize(parameters)}")
-            : executionId;
-    }
+	public async Task<string> ImportFromPackage(ImportFromPackageRequest parameters, CancellationToken cancellationToken = default)
+	{
+		string executionId = await PostRequestAsync<ImportFromPackageRequest, string>(nameof(ImportFromPackage),
+																				parameters,
+																				cancellationToken);
+		return executionId != parameters.ExecutionId
+			? throw new Exception($"The returned execution id ({executionId}), is not the same as the one defined in the parameters : {parameters.ExecutionId}.\nParameters : {JsonSerializer.Serialize(parameters)}")
+			: executionId;
+	}
 }
--- Dynamics365ImportData/Dynamics365ImportData/Erp/Dynamics365FnoClient.cs
+++ Dynamics365ImportData/Dynamics365ImportData/Erp/Dynamics365FnoClient.cs
@@ -13,129 +13,132 @@ using System.Text.Json;
 
 public abstract class Dynamics365FnoClient
 {
-    private const string _dataPath = "data";
-    private const string _domain = "Microsoft.Dynamics.DataEntities";
-    private readonly HttpClient _client;
-    private readonly string _clientId;
-    private readonly string _entity;
-    private readonly ILogger _logger;
-    private readonly string _secret;
-    private readonly string _tenant;
-    private readonly Uri _url;
-    private IConfidentialClientApplication? _confidentialApplication;
+	private const string _dataPath = "data";
+	private const string _domain = "Microsoft.Dynamics.DataEntities";
+	private readonly HttpClient _client;
+	private readonly string _clientId;
+	private readonly string _entity;
+	private readonly ILogger _logger;
+	private readonly string _secret;
+	private readonly string _tenant;
+	private readonly Uri _url;
+	private IConfidentialClientApplication? _confidentialApplication;
 
-    protected Dynamics365FnoClient(string entity, IOptions<Dynamics365Settings> settings, HttpClient client, ILogger logger)
-    {
-        _entity = entity;
-        _client = client ?? throw new ArgumentNullException(nameof(client));
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        if (string.IsNullOrWhiteSpace(settings.Value.Url?.OriginalString))
-        {
-            throw new ArgumentException($"The {nameof(settings.Value.Url)} setting is not defined.",
-                                        nameof(settings));
-        }
-        if (string.IsNullOrWhiteSpace(settings.Value.Tenant))
-        {
-            throw new ArgumentException($"The {nameof(settings.Value.Tenant)} setting is not defined.",
-                                        nameof(settings));
-        }
-        if (string.IsNullOrWhiteSpace(settings.Value.ClientId))
-        {
-            throw new ArgumentException($"The {nameof(settings.Value.ClientId)} setting is not defined.",
-                                        nameof(settings));
-        }
-        if (string.IsNullOrWhiteSpace(settings.Value.Secret))
-        {
-            throw new ArgumentException($"The {nameof(settings.Value.Secret)} setting is not defined.",
-                                        nameof(settings));
-        }
-        if (string.IsNullOrWhiteSpace(settings.Value.LegalEntityId))
-        {
-            throw new ArgumentException($"The {nameof(settings.Value.LegalEntityId)} setting is not defined.",
-                                        nameof(settings));
-        }
-        _url = settings.Value.Url;
-        _tenant = settings.Value.Tenant;
-        _clientId = settings.Value.ClientId;
-        _secret = settings.Value.Secret;
-        LegalEntityId = settings.Value.LegalEntityId;
-        client.BaseAddress = _url;
-    }
+	protected Dynamics365FnoClient(string entity, IOptions<Dynamics365Settings> settings, HttpClient client, ILogger logger)
+	{
+		_entity = entity;
+		_client = client ?? throw new ArgumentNullException(nameof(client));
+		_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		if (string.IsNullOrWhiteSpace(settings.Value.Url?.OriginalString))
+		{
+			throw new ArgumentException($"The {nameof(settings.Value.Url)} setting is not defined.",
+										nameof(settings));
+		}
+		if (string.IsNullOrWhiteSpace(settings.Value.Tenant))
+		{
+			throw new ArgumentException($"The {nameof(settings.Value.Tenant)} setting is not defined.",
+										nameof(settings));
+		}
+		if (string.IsNullOrWhiteSpace(settings.Value.ClientId))
+		{
+			throw new ArgumentException($"The {nameof(settings.Value.ClientId)} setting is not defined.",
+										nameof(settings));
+		}
+		if (string.IsNullOrWhiteSpace(settings.Value.Secret))
+		{
+			throw new ArgumentException($"The {nameof(settings.Value.Secret)} setting is not defined.",
+										nameof(settings));
+		}
+		if (string.IsNullOrWhiteSpace(settings.Value.LegalEntityId))
+		{
+			throw new ArgumentException($"The {nameof(settings.Value.LegalEntityId)} setting is not defined.",
+										nameof(settings));
+		}
+		_url = settings.Value.Url;
+		_tenant = settings.Value.Tenant;
+		_clientId = settings.Value.ClientId;
+		_secret = settings.Value.Secret;
+		LegalEntityId = settings.Value.LegalEntityId;
+		client.BaseAddress = _url;
+	}
 
-    public string LegalEntityId { get; }
+	public string LegalEntityId { get; }
 
-    private IConfidentialClientApplication ConfidentialApplication
-        => _confidentialApplication ??= ConfidentialClientApplicationBuilder
-            .Create(_clientId)
-            .WithAuthority(AzureCloudInstance.AzurePublic, _tenant)
-            .WithClientSecret(_secret)
-            .Build();
+	private IConfidentialClientApplication ConfidentialApplication
+		=> _confidentialApplication ??= ConfidentialClientApplicationBuilder
+			.Create(_clientId)
+			.WithAuthority(AzureCloudInstance.AzurePublic, _tenant)
+			.WithClientSecret(_secret)
+			.Build();
 
-    public async Task<string> AcquireTokenWithSecret(CancellationToken cancellationToken = default)
-    {
-        string[] scopes = new string[] { $"{_url.OriginalString}/.default" };
-        try
-        {
-            return (await ConfidentialApplication
-                  .AcquireTokenForClient(scopes)
-                  .ExecuteAsync(cancellationToken))
-                  .AccessToken;
-        }
-        catch
-        {
-            _logger.LogTrace("Error while acquiring Token.\nScope={Scope}\nAuthority={Authority}\nClientId={ClientId}", _url.OriginalString, ConfidentialApplication.Authority, _clientId);
-            throw;
-        }
-    }
+	public async Task<string> AcquireTokenWithSecret(CancellationToken cancellationToken = default)
+	{
+		string[] scopes = new string[] { $"{_url.OriginalString}/.default" };
+		try
+		{
+			return (await ConfidentialApplication
+				  .AcquireTokenForClient(scopes)
+				  .ExecuteAsync(cancellationToken))
+				  .AccessToken;
+		}
+		catch
+		{
+			_logger.LogTrace("Error while acquiring Token.\nScope={Scope}\nAuthority={Authority}\nClientId={ClientId}", _url.OriginalString, ConfidentialApplication.Authority, _clientId);
+			throw;
+		}
+	}
 
-    protected async Task<TResponse> PostRequestAsync<TRequest, TResponse>(string name, TRequest request, CancellationToken cancellationToken = default)
-    {
-        _client.DefaultRequestHeaders.Clear();
-        _client.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
-        _client.DefaultRequestHeaders.Add("OData-Version", "4.0");
-        _client.DefaultRequestHeaders.Add("Prefer", "odata.include-annotations = *");
-        _client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json")
-        );
-        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue
-            ("Bearer",
-            await AcquireTokenWithSecret(cancellationToken)
-        );
-        JsonContent content = JsonContent.Create(request);
+	protected async Task<TResponse> PostRequestAsync<TRequest, TResponse>(string name, TRequest request, CancellationToken cancellationToken = default)
+	{
+		_client.DefaultRequestHeaders.Clear();
+		_client.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
+		_client.DefaultRequestHeaders.Add("OData-Version", "4.0");
+		_client.DefaultRequestHeaders.Add("Prefer", "odata.include-annotations = *");
+		_client.DefaultRequestHeaders.Accept.Add(
+			new MediaTypeWithQualityHeaderValue("application/json")
+		);
+		_client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue
+			("Bearer",
+			await AcquireTokenWithSecret(cancellationToken)
+		);
+		var content = JsonContent.Create(request);
 
-        string url = $"/{_dataPath}/{_entity}/{_domain}.{name}";
-        HttpResponseMessage response = await _client
-            .PostAsync(url, content, cancellationToken);
-        if (response.IsSuccessStatusCode)
-        {
-            _logger.LogDebug("The method call to '{Path}' was succesfull.", url);
-            try
-            {
-                ODataResponse value = (await response
-                    .Content
-                    .ReadFromJsonAsync<ODataResponse>
-                    (options: new JsonSerializerOptions()
-                    {
-                        PropertyNameCaseInsensitive = true
-                    },
-                    cancellationToken))
-                    ?? throw new Exception($"The API Call '{name}' returned an empty body.");
-                return typeof(TResponse) == typeof(string)
-                    ? (TResponse)(object)(value.Value ?? string.Empty)
-                    : string.IsNullOrWhiteSpace(value.Value)
-                    ? throw new Exception($"The API Call '{name}' returned an empty OData value.")
-                    : JsonSerializer.Deserialize<TResponse>(
-                        value.Value)
-                            ?? throw new Exception($"Could not deserialize '{name}' returned OData value.\nType {typeof(TResponse)}\nValue : {value.Value}");
-            }
-            catch
-            {
-                _logger.LogError("Failed to read response content :\n{ResponseContent}", await response.Content.ReadAsStringAsync(cancellationToken));
-                throw;
-            }
-        }
-        string requestContent = await content.ReadAsStringAsync(cancellationToken);
-        string responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
-        throw new Exception($"The API Call was not succesful and returned status : {response.StatusCode}.\nReason : {response.ReasonPhrase}\nRequestContent : {requestContent}\nResponseContent : {responseContent}");
-    }
+		string url = $"/{_dataPath}/{_entity}/{_domain}.{name}";
+		HttpResponseMessage response = await _client
+			.PostAsync(url, content, cancellationToken);
+		_logger.LogDebug("POST {Url}\nRequest Body : {RequestBody}",
+			url,
+			await content.ReadAsStringAsync(cancellationToken));
+		if (response.IsSuccessStatusCode)
+		{
+			_logger.LogDebug("The method call to '{Path}' was succesfull.", url);
+			try
+			{
+				ODataResponse value = (await response
+					.Content
+					.ReadFromJsonAsync<ODataResponse>
+					(options: new JsonSerializerOptions()
+					{
+						PropertyNameCaseInsensitive = true
+					},
+					cancellationToken))
+					?? throw new Exception($"The API Call '{name}' returned an empty body.");
+				return typeof(TResponse) == typeof(string)
+					? (TResponse)(object)(value.Value ?? string.Empty)
+					: string.IsNullOrWhiteSpace(value.Value)
+					? throw new Exception($"The API Call '{name}' returned an empty OData value.")
+					: JsonSerializer.Deserialize<TResponse>(
+						value.Value)
+							?? throw new Exception($"Could not deserialize '{name}' returned OData value.\nType {typeof(TResponse)}\nValue : {value.Value}");
+			}
+			catch
+			{
+				_logger.LogError("Failed to read response content :\n{ResponseContent}", await response.Content.ReadAsStringAsync(cancellationToken));
+				throw;
+			}
+		}
+		string requestContent = await content.ReadAsStringAsync(cancellationToken);
+		string responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
+		throw new Exception($"The API Call was not succesful and returned status : {response.StatusCode}.\nReason : {response.ReasonPhrase}\nRequestContent : {requestContent}\nResponseContent : {responseContent}");
+	}
 }
